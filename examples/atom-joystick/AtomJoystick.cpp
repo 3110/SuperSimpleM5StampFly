79dc28bdd9591b5849a12b73f3783dc61fc74000
SAITO Tetsuya <saito.tetsuya@gmail.com>
2024-08-18 15:18:40 +0900

    I2C系のAPIを実装

--- examples/atom-joystick/AtomJoystick.cpp
+++ examples/atom-joystick/AtomJoystick.cpp
@@ -1,5 +1,7 @@
 #include "AtomJoystick.hpp"
 
+static constexpr char TAG[] = "AtomJoystick";
+
 const melody_tone_t AtomJoystick::STARTUP_MELODY[] = {
     {NOTE_D4, 200},
     {NOTE_A4, 200},
@@ -7,13 +9,60 @@ const melody_tone_t AtomJoystick::STARTUP_MELODY[] = {
     {NOTE_G4, 200},
 };
 
-AtomJoystick::AtomJoystick(uint8_t buzzerPin) : _melody(buzzerPin) {
+AtomJoystick::AtomJoystick(uint8_t buzzerPin)
+    : _wire(nullptr), _address(0), _melody(buzzerPin) {
 }
 
 AtomJoystick::~AtomJoystick(void) {
 }
 
-bool AtomJoystick::begin(void) {
+bool AtomJoystick::begin(TwoWire &wire, uint8_t address, uint8_t sda,
+                         uint8_t scl, uint32_t frequency) {
+    this->_wire = &wire;
+    this->_address = address;
+    this->_wire->begin(sda, scl, frequency);
+    delay(10);
+    uint8_t firmwareVersion;
+    if (!getFirmwareVersion(firmwareVersion)) {
+        ESP_LOGE(TAG, "Failed to initialize I2C");
+        return false;
+    }
+#if defined(CORE_DEBUG_LEVEL) && (CORE_DEBUG_LEVEL >= ESP_LOG_DEBUG)
+    uint8_t bootloaderVersion;
+    if (!getBootLoaderVersion(bootloaderVersion)) {
+        ESP_LOGE(TAG, "Failed to get bootloader version");
+        return false;
+    }
+    ESP_LOGD(TAG, "Version: Firmware %d, Bootloader %d", firmwareVersion,
+             bootloaderVersion);
+    float battery1Voltage = 0.0f;
+    if (!getBatteryVoltage(Battery::BATTERY_1, battery1Voltage)) {
+        ESP_LOGE(TAG, "Failed to get battery 1 voltage");
+        return false;
+    }
+    float battery2Voltage = 0.0f;
+    if (!getBatteryVoltage(Battery::BATTERY_2, battery2Voltage)) {
+        ESP_LOGE(TAG, "Failed to get battery 2 voltage");
+        return false;
+    }
+    ESP_LOGD(TAG, "Battery Voltage: (1) %3.2fV, (2) %3.2fV", battery1Voltage,
+             battery2Voltage);
+
+    uint16_t battery1Value = 0;
+    if (!getBatteryValue(Battery::BATTERY_1, adc_mode_t::ADC_12BITS,
+                         battery1Value)) {
+        ESP_LOGE(TAG, "Failed to get battery 1 value");
+        return false;
+    }
+    uint16_t battery2Value = 0;
+    if (!getBatteryValue(Battery::BATTERY_2, adc_mode_t::ADC_12BITS,
+                         battery2Value)) {
+        ESP_LOGE(TAG, "Failed to get battery 2 value");
+        return false;
+    }
+    ESP_LOGD(TAG, "Battery Value: (1) %d, (2) %d", battery1Value,
+             battery2Value);
+#endif
     return play(STARTUP_MELODY, _countof(STARTUP_MELODY));
 }
 
@@ -23,4 +72,69 @@ void AtomJoystick::beep(void) const {
 
 bool AtomJoystick::play(const melody_tone_t melody[], size_t size) const {
     return _melody.play(melody, size);
+}
+
+bool AtomJoystick::getFirmwareVersion(uint8_t &version) const {
+    return read(Register::FIRMWARE_VERSION, &version, sizeof(version));
+}
+
+bool AtomJoystick::getBootLoaderVersion(uint8_t &version) const {
+    return read(Register::BOOTLOADER_VERSION, &version, sizeof(version));
+}
+
+bool AtomJoystick::getBatteryVoltage(Battery battery, float &voltage) const {
+    uint8_t data[sizeof(voltage)];
+    if (!read(battery == Battery::BATTERY_1 ? Register::BATTERY1_VOLTAGE
+                                            : Register::BATTERY2_VOLTAGE,
+              data, sizeof(data))) {
+        ESP_LOGE(TAG, "Failed to read battery voltage");
+        return false;
+    }
+    voltage = ((data[1] << 8) | data[0]) * 0.001f;
+    return true;
+}
+
+bool AtomJoystick::getBatteryValue(Battery battery, adc_mode_t mode,
+                                   uint16_t &value) const {
+    uint8_t data[static_cast<uint8_t>(mode)];
+    if (!read(battery == Battery::BATTERY_1
+                  ? (mode == adc_mode_t::ADC_12BITS
+                         ? Register::BATTERY1_ADC_12BITS
+                         : Register::BATTERY1_ADC_8BITS)
+                  : (mode == adc_mode_t::ADC_12BITS
+                         ? Register::BATTERY2_ADC_12BITS
+                         : Register::BATTERY2_ADC_8BITS),
+              data, sizeof(data))) {
+        ESP_LOGE(TAG, "Failed to read battery value");
+        return false;
+    }
+    if (mode == adc_mode_t::ADC_12BITS) {
+        value = ((data[1] << 8) | data[0]);
+    } else {
+        value = data[0];
+    }
+    return true;
+}
+
+const char *AtomJoystick::getTag(void) const {
+    return TAG;
+}
+
+bool AtomJoystick::read(Register reg, uint8_t *data, size_t size) const {
+    this->_wire->beginTransmission(this->_address);
+    this->_wire->write(static_cast<uint8_t>(reg));
+    if (this->_wire->endTransmission() != 0) {
+        ESP_LOGE(TAG, "Failed to write to register 0x%02X",
+                 static_cast<uint8_t>(reg));
+        return false;
+    }
+    if (this->_wire->requestFrom(this->_address, size) != size) {
+        ESP_LOGE(TAG, "Failed to read data from register 0x%02X",
+                 static_cast<uint8_t>(reg));
+        return false;
+    }
+    for (size_t i = 0; i < size; i++) {
+        data[i] = this->_wire->read();
+    }
+    return true;
 }
--- examples/atom-joystick/AtomJoystick.hpp
+++ examples/atom-joystick/AtomJoystick.hpp
@@ -1,9 +1,67 @@
 #pragma once
 
+#include <Wire.h>
+#include <esp_log.h>
+
 #include "Melody.hpp"
 
 class AtomJoystick {
 public:
+    /**
+     * @brief I2Cアドレス
+     */
+    static constexpr uint8_t I2C_ADDRESS = 0x59;
+    /**
+     * @brief I2C周波数
+     */
+    static constexpr uint32_t I2C_FREQUENCY = 400000UL;
+    /**
+     * @brief レジスタ
+     */
+    enum class Register
+    {
+        JOY1_ADC_VALUE_128ITS = 0x00,
+        JOY1_ADC_VALUE_8BITS = 0x10,
+        JOY2_ADC_VALUE_128BITS = 0x20,
+        JOY2_ADC_VALUE_8BITS = 0x30,
+        BATTERY1_ADC_12BITS = 0x40,
+        BATTERY1_ADC_8BITS = 0x50,
+        BATTERY2_ADC_12BITS = 0x42,
+        BATTERY2_ADC_8BITS = 0x51,
+        BATTERY1_VOLTAGE = 0x60,
+        BATTERY2_VOLTAGE = 0x62,
+        BUTTON = 0x70,
+        BOOTLOADER_VERSION = 0xFC,
+        FIRMWARE_VERSION = 0xFE,
+        I2C_ADDRESS = 0xFF,
+    };
+    /**
+     * @brief ADCモード。
+     * @note 値はバイト数と合わせている
+     */
+    enum class adc_mode_t
+    {
+        ADC_8BITS = 1,
+        ADC_12BITS = 2,
+    };
+    /*
+     * ボタンの種類
+     */
+    enum class button_t
+    {
+        BUTTON_1 = 0,
+        BUTTON_2,
+        BUTTON_A,
+        BUTTON_B
+    };
+    /**
+     * @brief バッテリの位置
+     */
+    enum class Battery
+    {
+        BATTERY_1 = 0,
+        BATTERY_2,
+    };
     /**
      * @brief ブザーのピン番号
      */
@@ -25,9 +83,16 @@ public:
 
     /**
      * @brief 初期化を行います
+     * @param wire Wireオブジェクト
+     * @param address I2Cアドレス
+     * @param sda SDAピン番号
+     * @param scl SCLピン番号
+     * @param frequency I2C周波数
      * @return 初期化が成功した場合はtrue、失敗した場合はfalse
      */
-    virtual bool begin(void);
+    virtual bool begin(TwoWire &wire = Wire, uint8_t address = I2C_ADDRESS,
+                       uint8_t sda = SDA, uint8_t scl = SCL,
+                       uint32_t frequency = I2C_FREQUENCY);
     /**
      * @brief ビープ音を鳴らします
      */
@@ -40,6 +105,56 @@ public:
      */
     virtual bool play(const melody_tone_t melody[], size_t size) const;
 
+    /**
+     * @brief ファームウェアのバージョンを取得します
+     * @param version ファームウェアのバージョン
+     * @return 取得が成功した場合はtrue、失敗した場合はfalse
+     */
+    virtual bool getFirmwareVersion(uint8_t &version) const;
+
+    /**
+     * @brief ブートローダーのバージョンを取得します
+     * @param version ブートローダーのバージョン
+     * @return 取得が成功した場合はtrue、失敗した場合はfalse
+     */
+    virtual bool getBootLoaderVersion(uint8_t &version) const;
+
+    /**
+     * @brief バッテリーの電圧を取得します
+     * @param battery バッテリーの位置
+     * @param voltage バッテリーの電圧
+     * @return 取得が成功した場合はtrue、失敗した場合はfalse
+     */
+    virtual bool getBatteryVoltage(Battery battery, float &voltage) const;
+
+    /**
+     * @brief バッテリーの値を取得します
+     * @param battery バッテリーの位置
+     * @param mode モード
+     * @param value バッテリーの値
+     * @return 取得が成功した場合はtrue、失敗した場合はfalse
+     */
+    virtual bool getBatteryValue(Battery battery, adc_mode_t mode,
+                                 uint16_t &value) const;
+
+protected:
+    /**
+     * @brief ログ出力用のタグを取得します
+     * @return タグ
+     */
+    virtual const char *getTag(void) const;
+
+    /**
+     * @brief I2Cでデータを読み込みます
+     * @param register レジスタ
+     * @param data データ
+     * @param size データのサイズ
+     * @return 読み込みが成功した場合はtrue、失敗した場合はfalse
+     */
+    virtual bool read(Register register, uint8_t *data, size_t size) const;
+
 private:
+    TwoWire *_wire;
+    uint8_t _address;
     Melody _melody;
 };
